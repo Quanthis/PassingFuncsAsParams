e9cf9cc16e71e3a9c79dda2603cb6a44131befb4
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2019-12-16 08:08:33 +0100

    1

--- PassingFuncsAsParams/Program.cs
+++ PassingFuncsAsParams/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Numerics;
+using System.Threading;
+using static System.Console;
+using static System.Convert;
 
 namespace PassingFuncsAsParams
 {
     class Program
     {
+        private static void DoSomething()
+        {
+            WriteLine("Doing something...");
+            Thread.Sleep(5000);
+        }
+
+        private static BigInteger Silnia(int l)
+        {
+            BigInteger w;
+
+            for (BigInteger i = 1; i <= l; i++)
+            {
+                w *= i;
+            }
+            return w;
+        }
+
+        private static void DoingSomethingMore(Action ac)
+        {
+            DoSomething();
+        }
+
+        private static void SilniaZWynikiem(Func<int, BigInteger> ac, int n)
+        {
+            WriteLine(Silnia(n));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //DoingSomethingMore(DoSomething);
+            SilniaZWynikiem(Silnia, 10000);
         }
     }
 }
